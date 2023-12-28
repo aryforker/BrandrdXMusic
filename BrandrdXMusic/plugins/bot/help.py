4ffccdb47572c5fe992c8367495b2e3df0185c96
aryforker <97401553+aryforker@users.noreply.github.com>
2023-12-28 23:24:33 +0330

    شش

--- BrandrdXMusic/plugins/bot/help.py
+++ BrandrdXMusic/plugins/bot/help.py
@@ -40,7 +40,7 @@ async def helper_private(
         keyboard = help_pannel(_)
         
         await update.reply_video(
-            video="https://graph.org/file/84d30d4fd04570c0e0256.mp4",
+            video="https://telegra.ph/file/1c9073bda024c361df6c0.mp4",
             caption=_["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard)
 
 
